dbe3fcb1f1dbc657dd1ac9b91b2b49c1ab169a9c
jyost <jyost@DESKTOP-G3LDKBE>
2020-07-28 16:57:01 +0530

    Laguage BAsics

---
+++ Milestone1/src/javaFundamentals/languageBasics/Example_3.java
@@ -0,0 +1,13 @@
+package javaFundamentals.languageBasics;
+
+public class Example_3 {
+	public static void main(String args[])
+	{
+		int a,b;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		 System.out.println("The Sum of "+a+" and "+b+" is "+(a+b));
+		
+	}
+
+}
